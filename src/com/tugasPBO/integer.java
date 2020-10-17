4b60304a86db119410fca0cf5c0ecf100e1a518f
unknown <argya.programmer@gmail.com>
2020-10-17 14:36:24 +0700

    Initial Commit

---
+++ src/com/tugasPBO/integer.java
@@ -0,0 +1,19 @@
+//latihan 7 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan bilangan berbentuk angka dan jenis-jenis tipe data bilangan bulat
+package com.tugasPBO;
+
+public class integer {
+    public static void main(String[] args){
+        byte b = 120;
+        short s = 32767;
+        int i = 1_000_000_000;
+        long l = 90000000000L;
+        System.out.println("byte: "+ b);
+        System.out.println("short: "+ s);
+        System.out.println("int: "+i);
+        System.out.println("long: "+l);
+    }
+}
